a907e6b0de500cbeaef9dc5f6ec4b6132ac289a8
cyptt <254813752@qq.com>
2021-02-04 10:48:02 +0800

    ch--提交

---
+++ haveIg/haveIg/Testdddd.h
@@ -0,0 +1,16 @@
+//
+//  Testdddd.h
+//  haveIg
+//
+//  Created by cyptt on 2021/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Testdddd : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ haveIg/haveIg/Testdddd.m
@@ -0,0 +1,15 @@
+//
+//  Testdddd.m
+//  haveIg
+//
+//  Created by cyptt on 2021/2/4.
+//
+
+#import "Testdddd.h"
+
+@implementation Testdddd
+
++(void)test{
+    
+}
+@end
